406696904e6088c11923cbd24c4a40510f5b9b8b
queena888 <queena888@users.noreply.github.com>
2017-03-08 16:54:35 -0700

    Add files via upload

--- checkpoint2/spffdec.c
+++ checkpoint2/spffdec.c
@@ -1,6 +1,9 @@
 /*
- * spff image format decoder, code based on BMP format
- * Copyright (c) 2005 Mans Rullgard
+ * CS 3505 Spring 2017
+ * SPFF image format decoder
+ * only handle image in RBG8 color
+ * Code base on bmp format in ffmpeg
+ * By Minh Pham and To Tang
  *
  * This file is part of FFmpeg.
  *
@@ -36,28 +39,26 @@ static int spff_decode_frame(AVCodecContext *avctx,
     unsigned int fsize, hsize;
     int width, height;
     unsigned int bit_count;
+   
     unsigned int ihsize;
     int i, n, linesize, ret;
-    //   uint32_t alpha = 0;
+
     uint8_t *ptr;
     int dsize;
     const uint8_t *buf0 = buf;
 
-     // print out CS 3505 stuff
-    static int been_here  = 0;
-    if(!been_here)
-      av_log(avctx,AV_LOG_INFO, "\n*** CS 3505: Executing in %s and %s***\n*** CS 3505: Modified by To Tang and Minh Pham *** \n ","spff_decode_frame","spffdec.c");
-    been_here = 1;
     if (buf_size < 10) {
         av_log(avctx, AV_LOG_ERROR, "buf size too small (%d)\n", buf_size);
         return AVERROR_INVALIDDATA;
     }
+
     if (bytestream_get_byte(&buf) != 'S' ||
         bytestream_get_byte(&buf) != 'F') {
         av_log(avctx, AV_LOG_ERROR, "bad magic number\n");
         return AVERROR_INVALIDDATA;
     }
-    fsize = bytestream_get_le32(&buf);
+
+    fsize = bytestream_get_le32(&buf); // filesize = image size + header size
     if (buf_size < fsize) {
         av_log(avctx, AV_LOG_ERROR, "not enough data (%d < %u), trying to decode anyway\n",
                buf_size, fsize);
@@ -66,8 +67,9 @@ static int spff_decode_frame(AVCodecContext *avctx,
     if(avctx->width < 0 || avctx->height <0){
        av_log(avctx, AV_LOG_ERROR, "negative width or height\n");
     }
-    hsize  = bytestream_get_le32(&buf); /* header size */
-    ihsize = bytestream_get_le32(&buf); /* more header size */
+    
+    hsize  = bytestream_get_le32(&buf); /* fileheader+infoheader size */
+    ihsize = bytestream_get_le32(&buf); /* info header size */
     if (ihsize + 10LL > hsize) {
         av_log(avctx, AV_LOG_ERROR, "invalid header size %u\n", hsize);
         return AVERROR_INVALIDDATA;
@@ -83,19 +85,26 @@ static int spff_decode_frame(AVCodecContext *avctx,
                fsize, hsize);
         return AVERROR_INVALIDDATA;
     }
-    width  = bytestream_get_le32(&buf);
-    height = bytestream_get_le32(&buf);
+
+    width  = bytestream_get_le32(&buf); // get width
+    height = bytestream_get_le32(&buf); // get height
+   
+
     /* planes */
-    if (bytestream_get_le16(&buf) != 1) {
+    if (bytestream_get_le16(&buf) != 1) { // make sure only 1 plane
         av_log(avctx, AV_LOG_ERROR, "invalid SPFF header\n");
         return AVERROR_INVALIDDATA;
     }
-    bit_count = bytestream_get_le16(&buf); // bit_count
+
+    bit_count = bytestream_get_le16(&buf); // bit_count always =8 (RBG8)
+
+   
     avctx->width  = width;
-    avctx->height = height > 0 ? height : -height;
-    avctx->pix_fmt = AV_PIX_FMT_NONE;
+    avctx->height = height > 0 ? height : -height; // make sure height is positive
+    avctx->pix_fmt = AV_PIX_FMT_NONE; // set default pixel format
+
     if(bit_count == 8)
-      avctx->pix_fmt = AV_PIX_FMT_BGR8;
+      avctx->pix_fmt = AV_PIX_FMT_BGR8; // set pixel format to BGR8
     else
       {
         av_log(avctx, AV_LOG_ERROR, "depth %u not supported\n", bit_count);
@@ -105,15 +114,20 @@ static int spff_decode_frame(AVCodecContext *avctx,
         av_log(avctx, AV_LOG_ERROR, "unsupported pixel format\n");
         return AVERROR_INVALIDDATA;
     }
+
+     // ff_get_buffer(AVCodecContext, AVFrame, int), get buffer for a frame
     if ((ret = ff_get_buffer(avctx, p, 0)) < 0)
         return ret;
-    p->pict_type = AV_PICTURE_TYPE_I;
+
+    p->pict_type = AV_PICTURE_TYPE_I; // frame's picture type = Intra
     p->key_frame = 1;
+
     buf   = buf0 + hsize;
     dsize = buf_size - hsize;
+
     /* Line size in file multiple of 4 */
     n = ((avctx->width * bit_count + 31) / 8) & ~3;
-    if (n * avctx->height > dsize ) {
+    if (n * avctx->height > dsize) {
         n = (avctx->width * bit_count + 7) / 8;
         if (n * avctx->height > dsize) {
             av_log(avctx, AV_LOG_ERROR, "not enough data (%d < %d)\n",
@@ -122,12 +136,17 @@ static int spff_decode_frame(AVCodecContext *avctx,
         }
         av_log(avctx, AV_LOG_ERROR, "data size too small, assuming missing line alignment\n");
     }
+    // set pointer
     ptr      = p->data[0] + (avctx->height - 1) * p->linesize[0];
     linesize = -p->linesize[0];
+
     if(bit_count == 8)
       {
 	for (i = 0; i < avctx->height; i++) {
-	  memcpy(ptr, buf, n);
+	  // copies count bytes from the object pointed to by src to the object 
+	  // pointed to by dest. memcpy(destination, source, count bytes)
+	  memcpy(ptr, buf, n); 
+	  //move pointers
 	  buf += n;
 	  ptr += linesize;
 	}
--- checkpoint2/spffenc.c
+++ checkpoint2/spffenc.c
@@ -1,7 +1,9 @@
 /*
- * BMP image format encoder
- * Copyright (c) 2006, 2007 Michel Bardiaux
- * Copyright (c) 2009 Daniel Verkamp <daniel at drv.nu>
+ * CS 3505 Spring 2017
+ * SPFF image format encoder
+ * only handle image in RBG8 color
+ * Code base on bmp format in ffmpeg
+ * By Minh Pham and To Tang
  *
  * This file is part of FFmpeg.
  *
@@ -22,30 +24,32 @@
 
 #include "libavutil/imgutils.h"
 #include "libavutil/avassert.h"
-#include "avcodec.h"
 #include "bytestream.h"
+#include "avcodec.h"
 #include "internal.h"
 
+// Get AVCodecContext and make sure it is the correct color scheme
 static av_cold int spff_encode_init(AVCodecContext *avctx){
   if(avctx->pix_fmt == AV_PIX_FMT_RGB8||avctx->pix_fmt == AV_PIX_FMT_BGR8)
     {
       avctx->bits_per_coded_sample = 8;
     }
   else {
-    av_log(avctx, AV_LOG_INFO, "unsupported pixel format\n");
+    av_log(avctx, AV_LOG_INFO, "unsupported pixel format, only support RBG8\n");
     return AVERROR(EINVAL);
   }
   return 0;
 }
 
+// encode one frame, spff only support one frame only
 static int spff_encode_frame(AVCodecContext *avctx, AVPacket *pkt,
 			     const AVFrame *pict, int *got_packet)
 {
   const AVFrame * const p = pict;
   int n_bytes_image, n_bytes_per_row, n_bytes, i, hsize, ret;
-  const uint32_t *pal = NULL;
-  uint32_t palette256[256];
   int pad_bytes_per_row, pal_entries = 0;
+  const uint32_t *pal = NULL;
+   uint32_t palette256[256];
   int bit_count = avctx->bits_per_coded_sample;
   uint8_t *ptr, *buf;
 
@@ -55,51 +59,59 @@ static int spff_encode_frame(AVCodecContext *avctx, AVPacket *pkt,
   avctx->coded_frame->key_frame = 1;
   FF_ENABLE_DEPRECATION_WARNINGS
 #endif
+   av_assert1(bit_count == 8); // assert correct color scheme: RGB8
+  //assign RGB values into palette256
+   avpriv_set_systematic_pal2(palette256, avctx->pix_fmt);
+   pal = palette256; // rereference pal
+   
+   if (pal && !pal_entries) pal_entries = 1 << bit_count;
+   n_bytes_per_row = (int64_t)avctx->width;
+   pad_bytes_per_row = (4 - n_bytes_per_row) & 3;
+   //calculate image size
+   n_bytes_image = avctx->height * (n_bytes_per_row + pad_bytes_per_row);
 
-  av_assert1(bit_count == 8);
-  avpriv_set_systematic_pal2(palette256, avctx->pix_fmt);
-  pal = palette256;
-
-  if (pal && !pal_entries) pal_entries = 1 << bit_count;
-  n_bytes_per_row = ((int64_t)avctx->width * (int64_t)1);
-  pad_bytes_per_row = (4 - n_bytes_per_row) & 3;
-  n_bytes_image = avctx->height * (n_bytes_per_row + pad_bytes_per_row);
-  // STRUCTURE.field refer to the MSVC documentation for BITMAPFILEHEADER
+  // STRUCTURE.field refer to the MSVC documentation for SPFFFILEHEADER
   // and related pages.
 #define SIZE_SPFFFILEHEADER 10
-#define SIZE_SPFFINFOHEADER 20
+#define SIZE_SPFFINFOHEADER 16
+   // calculate header size = fileheader size + infoheader size
   hsize = SIZE_SPFFFILEHEADER + SIZE_SPFFINFOHEADER + (pal_entries << 2);
-  n_bytes = n_bytes_image + hsize;
-  //Check AVPacket size and/or allocate data.
+  n_bytes = n_bytes_image + hsize; // calculate filesize=header size+image size
+   //Check AVPacket size and/or allocate data.
   if ((ret = ff_alloc_packet2(avctx, pkt, n_bytes, 0)) < 0)
     return ret;
   buf = pkt->data;
-  bytestream_put_byte(&buf, 'S');                   // BITMAPFILEHEADER.bfType
+  bytestream_put_byte(&buf, 'S');                   // SPFFFILEHEADER.bfType
   bytestream_put_byte(&buf, 'F');                   // do.
-  bytestream_put_le32(&buf, n_bytes);               // BITMAPFILEHEADER.bfSize
-  bytestream_put_le32(&buf, hsize);                 // BITMAPFILEHEADER.bfOffBits
-  bytestream_put_le32(&buf, SIZE_SPFFINFOHEADER);   // BITMAPINFOHEADER.biSize
-  bytestream_put_le32(&buf, avctx->width);          // BITMAPINFOHEADER.biWidth
-  bytestream_put_le32(&buf, avctx->height);         // BITMAPINFOHEADER.biHeight
-  bytestream_put_le16(&buf, 1);                     // BITMAPINFOHEADER.biPlanes
+  bytestream_put_le32(&buf, n_bytes);               // SPFFFILEHEADER.bfSize
+  bytestream_put_le32(&buf, hsize);                 // SPFFFILEHEADER.bfOffBits
+
+  bytestream_put_le32(&buf, SIZE_SPFFINFOHEADER);   // SPFFINFOHEADER.biSize
+  bytestream_put_le32(&buf, avctx->width);          // SPFFINFOHEADER.biWidth
+  bytestream_put_le32(&buf, avctx->height);         // SPFFINFOHEADER.biHeight
+  bytestream_put_le16(&buf, 1);                     // SPFFINFOHEADER.biPlanes
   // we choose to represent each pixel with 
   // 3 bytes for red,green,blue. bit_count should be 24
-  bytestream_put_le16(&buf, bit_count);             // BITMAPINFOHEADER.biBitCount
-  //bytestream_put_le32(&buf, n_bytes_image);         // BITMAPINFOHEADER.biSizeImage
-  for (i = 0; i < pal_entries; i++)
-    bytestream_put_le32(&buf, pal[i] & 0xFFFFFF);
-  // BMP files are bottom-to-top so we start from the end...
+  bytestream_put_le16(&buf, bit_count);             // SPFFINFOHEADER.biBitCount
+  
+  // SPFF files are bottom-to-top so we start from the end...
   ptr = p->data[0] + (avctx->height - 1) * p->linesize[0];
   buf = pkt->data + hsize;
   for(i = 0; i < avctx->height; i++) {
-    memcpy(buf, ptr, n_bytes_per_row);
+    //bit_count is always 8
+    {
+      // copies count bytes from the object pointed to by src to the object 
+      // pointed to by dest. memcpy(destination, source, count bytes)
+      memcpy(buf, ptr, n_bytes_per_row);
+    }
+    // move pointers
     buf += n_bytes_per_row;
-    memset(buf, 0, pad_bytes_per_row);
+    memset(buf, 0, pad_bytes_per_row);// put pad_bytes_per_row 0's into buf
     buf += pad_bytes_per_row;
     ptr -= p->linesize[0]; // ... and go back
   }
 
-  pkt->flags |= AV_PKT_FLAG_KEY;
+  pkt->flags |= AV_PKT_FLAG_KEY; // bitwise OR for flag values
   *got_packet = 1;
   return 0;
 }
@@ -111,5 +123,5 @@ AVCodec ff_spff_encoder = {
   .id             = AV_CODEC_ID_SPFF,
   .init           = spff_encode_init,
   .encode2        = spff_encode_frame,
-  .pix_fmts       = (const enum AVPixelFormat[]){ AV_PIX_FMT_RGB8, AV_PIX_FMT_BGR8,AV_PIX_FMT_NONE },
+  .pix_fmts       = (const enum AVPixelFormat[]){AV_PIX_FMT_RGB8, AV_PIX_FMT_BGR8,AV_PIX_FMT_NONE},
 };
